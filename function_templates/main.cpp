8d0f611e965f9447be33a9d2ff30c1cc48768f4b
Andres Vicente <andres.vicente.arevalo@gmail.com>
2023-03-14 10:07:53 +0000

    added templates finish

--- function_templates/main.cpp
+++ function_templates/main.cpp
@@ -40,9 +40,21 @@ template <typename T> T minimum(T a, T b);
 // but some types may not have the < operator defined, so we need to make sure that the types we use
 // have the operatorors defined
 
+// template with type parameters as reference
+template <typename T> const T& minimum_ref(const T& a, const T& b);
+
 // for example the multiplication operator * is defined for all the numeric types, but not for strings
 template <typename T> T multiply(T a, T b) {
     return a * b;
+}
+
+// We can also use template specialization to overwrite the default template behavior for a specific type
+// for example we can define a template specialization for the multiply function for const char*
+// the syntax is:
+template <> const char* multiply<const char*>(const char* a, const char* b) {
+    std::string res{a};
+    res += b;
+    return res.c_str();
 }
 
 
@@ -51,7 +63,7 @@ int main() {
     // define a few variables to test the functions
     int a = 5;
     int b = 10;
-    double c = 5.5;
+    double c = 4.5;
     double d = 10.5;
     float e = 5.5f;
     float f = 10.5f;
@@ -81,12 +93,51 @@ int main() {
 
     // we can also use the debuger to see the generated code for the templates and to debug the templates
 
+    // Template type deduction is the process of deducing the type of a template parameter from the type of the
+    // argument passed to the template function or template class
+
+    // to aboid unwanted typecasting we can explicitly specify the type of the template parameter to control the
+    // type deduction process (template argument deduction explicitly)
+    auto res_int = minimum<int>(a, b); //(int version call of the function template)
+    // with this we can also do implicit conversions of the arguments and use int, float, double, combined
+    auto res_float = minimum<float>(a, c); //(float version call of the function template)
+
+    std::cout << "minimum<int>(int, int) = " << res_int << std::endl;
+    std::cout << "minimum<float>(int, double) = " << res_float << std::endl;
+
+    // we can also pass the template type parameters by value or by reference
+    std::cout << "minimum_ref(int, int) = " << minimum_ref(a, b) << std::endl;
+    // with a and b
+    std::cout << "&a = " << &a << ", &b = " << &b << std::endl;
+
+    // we can also use it with other types
+    std::string i{"hello"};
+    std::string j{"world"};
+    std::cout << "minimum_ref(std::string, std::string) = " << minimum_ref(i, j) << std::endl;
+
+
+    // We can also use template specialization to overwrite the default template behavior for a specific type
+    // for example we can define a template specialization for the multiply function for strings
+    const char* s{"qwerty"};
+    const char* t{"asd"};
+
+    const char* result = multiply(s,t);
+    std::cout << "multiply(const char*) = " << result << std::endl;
+
     std::cout << "END" << std::endl;
     return 0;
-}
+}   
 
 
 // and here we define the function template
 template <typename T> T minimum(T a, T b) {
+    return a < b ? a : b;
+}
+
+template <typename T> const T& minimum_ref(const T& a, const T& b) {
+    std::cout << "minimum_ref = " << &a << ", " << &b << std::endl;
+    // if you try to modify the value of a or b you will get a compile error
+    // because the function is declared as const
+    // a = 10;
     return a < b ? a : b;
 }
